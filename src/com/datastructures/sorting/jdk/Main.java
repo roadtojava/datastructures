ab53ab2df7bc56ca54415c8625e536d481cf029d
roadtojava <nihad.akbarli@idrak.az>
2022-12-23 15:44:59 +0400

    new commit

---
+++ src/com/datastructures/sorting/jdk/Main.java
@@ -0,0 +1,16 @@
+package com.datastructures.sorting.jdk;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        Arrays.parallelSort(intArray);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
